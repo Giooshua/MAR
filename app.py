c3422c91da8318a2728d10ddeb6700a6bdc0fc0b
Giovanni <itsgiovannipalma@gmail.com>
2024-11-04 12:03:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,16 @@ st.image("https://i.ibb.co/g6k3gvC/mar-high-resolution-logo-4.png", width=200)
 st.title("Interfaccia")
 
 # Inizializza lo stato della sessione
-if 'proceed_to_step_2' not in st.session_state:
-    st.session_state['proceed_to_step_2'] = False
-if 'selected_variable' not in st.session_state:
-    st.session_state['selected_variable'] = None
-if 'raggruppate_altro' not in st.session_state:
-    st.session_state['raggruppate_altro'] = {}
-if 'proceed_to_step_3' not in st.session_state:
-    st.session_state['proceed_to_step_3'] = False
+if 'current_step' not in st.session_state:
+    st.session_state['current_step'] = 1
 if 'dataset' not in st.session_state:
     st.session_state['dataset'] = None
 if 'uploaded_file' not in st.session_state:
     st.session_state['uploaded_file'] = None
+if 'selected_variable' not in st.session_state:
+    st.session_state['selected_variable'] = None
+if 'raggruppate_altro' not in st.session_state:
+    st.session_state['raggruppate_altro'] = {}
 
 # Funzione per categorizzare le variabili
 def categorize_variable(dataset, column):
@@ -42,50 +40,52 @@ def categorize_variable(dataset, column):
 
 # STEP 1: Caricamento del Dataset
 # ----------------------------------------
-with st.expander("Step 1: Caricamento del Dataset", expanded=True):
-    uploaded_file = st.file_uploader("Carica il tuo dataset (.csv, .txt)", type=["csv", "txt"])
-    
-    if uploaded_file is not None:
-        st.session_state['uploaded_file'] = uploaded_file  # Salva il file in session_state
+if st.session_state['current_step'] == 1:
 
-    def load_dataset(uploaded_file, delimiter=','):
-        try:
-            if uploaded_file.name.endswith('.csv') or uploaded_file.name.endswith('.txt'):
-                df = pd.read_csv(uploaded_file, delimiter=delimiter)
-            else:
+    with st.expander("Step 1: Caricamento del Dataset", expanded=True):
+        uploaded_file = st.file_uploader("Carica il tuo dataset (.csv, .txt)", type=["csv", "txt"])
+
+        if uploaded_file is not None:
+            st.session_state['uploaded_file'] = uploaded_file  # Salva il file in session_state
+
+        def load_dataset(uploaded_file, delimiter=','):
+            try:
+                if uploaded_file.name.endswith('.csv') or uploaded_file.name.endswith('.txt'):
+                    df = pd.read_csv(uploaded_file, delimiter=delimiter)
+                else:
+                    return None
+                return df
+            except Exception as e:
+                st.error(f"Errore nel caricamento del file: {str(e)}")
                 return None
-            return df
-        except Exception as e:
-            st.error(f"Errore nel caricamento del file: {str(e)}")
-            return None
 
-    if st.session_state['uploaded_file'] is not None:
-        if st.session_state['dataset'] is None:
-            dataset = load_dataset(st.session_state['uploaded_file'])
-            if dataset is not None:
-                st.session_state['dataset'] = dataset  # Salva il dataset in session_state
-                st.success(f"Dataset caricato con successo! Righe: {dataset.shape[0]}, Colonne: {dataset.shape[1]}")
+        if st.session_state['uploaded_file'] is not None:
+            if st.session_state['dataset'] is None:
+                dataset = load_dataset(st.session_state['uploaded_file'])
+                if dataset is not None:
+                    st.session_state['dataset'] = dataset  # Salva il dataset in session_state
+                    st.success(f"Dataset caricato con successo! Righe: {dataset.shape[0]}, Colonne: {dataset.shape[1]}")
+                    st.write(dataset.head())
+
+                    # Chiedi se l'utente vuole passare allo Step 2
+                    if st.button("Panoramica Esplorativa del Dataset"):
+                        st.session_state['current_step'] = 2
+                else:
+                    st.error("Caricamento del dataset fallito. Verifica il file e riprova.")
+            else:
+                dataset = st.session_state['dataset']
+                st.success(f"Dataset già caricato! Righe: {dataset.shape[0]}, Colonne: {dataset.shape[1]}")
                 st.write(dataset.head())
 
                 # Chiedi se l'utente vuole passare allo Step 2
                 if st.button("Panoramica Esplorativa del Dataset"):
-                    st.session_state['proceed_to_step_2'] = True
-            else:
-                st.error("Caricamento del dataset fallito. Verifica il file e riprova.")
+                    st.session_state['current_step'] = 2
         else:
-            dataset = st.session_state['dataset']
-            st.success(f"Dataset già caricato! Righe: {dataset.shape[0]}, Colonne: {dataset.shape[1]}")
-            st.write(dataset.head())
-
-            # Chiedi se l'utente vuole passare allo Step 2
-            if st.button("Panoramica Esplorativa del Dataset"):
-                st.session_state['proceed_to_step_2'] = True
-    else:
-        st.info("Per favore, carica un dataset per procedere.")
+            st.info("Per favore, carica un dataset per procedere.")
 
 # STEP 2: Panoramica Esplorativa del Dataset
 # ----------------------------------------
-if st.session_state['proceed_to_step_2'] and not st.session_state['proceed_to_step_3'] and st.session_state['dataset'] is not None:
+elif st.session_state['current_step'] == 2 and st.session_state['dataset'] is not None:
     dataset = st.session_state['dataset']  # Recupera il dataset da session_state
     with st.expander("Step 2: Panoramica Esplorativa del Dataset", expanded=True):
         with st.spinner('Caricamento in corso...'):
@@ -155,12 +155,11 @@ if st.session_state['proceed_to_step_2'] and not st.session_state['proceed_to_st
 
         # Pulsante per passare allo Step 3
         if st.button("Analisi dell'Entità dei Dati Mancanti", key='step_3_button'):
-            st.session_state['proceed_to_step_3'] = True
-            st.session_state['proceed_to_step_2'] = False  # Resetta lo stato dello Step 2
+            st.session_state['current_step'] = 3
 
 # STEP 3: Analisi dell'Entità dei Dati Mancanti
 # ----------------------------------------
-if st.session_state['proceed_to_step_3'] and st.session_state['dataset'] is not None:
+elif st.session_state['current_step'] == 3 and st.session_state['dataset'] is not None:
     dataset = st.session_state['dataset']  # Recupera il dataset da session_state
     with st.expander("Step 3: Analisi dell'Entità dei Dati Mancanti", expanded=True):
         with st.spinner('Analisi dei dati mancanti in corso...'):
@@ -209,3 +208,6 @@ if st.session_state['proceed_to_step_3'] and st.session_state['dataset'] is not 
                 st.pyplot(plt)
             else:
                 st.write("Non ci sono abbastanza dati mancanti per analizzare i pattern di missingness.")
+
+# Aggiungi ulteriori step se necessario, utilizzando lo stesso approccio
+
